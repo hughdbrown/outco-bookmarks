255cddcf39ca21094b0fde2112025fde3d1c4c7f
Hugh Brown <hughdbrown@yahoo.com>
2020-04-22 09:46:01 -0600

    Add urls.js

---
+++ urls.js
@@ -0,0 +1,27 @@
+export const urls = [
+    // Other stuff
+    "https://codeinterview.io/",
+    "https://airtable.com/",
+
+    // Outco urls
+    "https://outco.io/interviewfeedback",
+    "https://outco.io/interviewrecap",
+    "https://outco.io/offers",
+    "https://outco.io/outcodersupport",
+    "https://outco.io/reflections",
+
+    // bit.ly urls
+    "http://bit.ly/outco-online1",
+    "http://bit.ly/outco-outcomes",
+    "http://bit.ly/outcointerviewprep",
+    "http://bit.ly/outcojobresources",
+
+    // Compensation
+    "https://www.levels.fyi/#",
+
+    // Tech interview
+    "https://hackernoon.com/14-patterns-to-ace-any-coding-interview-question-c5bb3357f6ed"
+];
+
+
+export const folderName = "Outco";
